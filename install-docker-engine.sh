eb79443ca2d7c868132bb3c3e6b2ecf26f82fa78
awkirin <awkirin@gmail.com>
2025-07-12 01:23:21 +0300

    update

--- install-docker-engine.sh
+++ install-docker-engine.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/awkirin/awk-scripts/HEAD/install-docker-engine.sh)"
+
+
 # Run the following command to uninstall all conflicting packages:
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
     sudo apt remove $pkg;
