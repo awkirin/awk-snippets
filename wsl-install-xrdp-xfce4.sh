c5a89e215dac1bb970ad439e5568906a240feb19
awkirin <awkirin@gmail.com>
2025-07-12 01:46:45 +0300

    update

--- wsl-install-xrdp-xfce4.sh
+++ wsl-install-xrdp-xfce4.sh
@@ -10,7 +10,7 @@ sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
 sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
 echo xfce4-session > ~/.xsession
 
-sed -i 's|^test -x /etc/X11/Xsession && exec /etc/X11/Xsession|# &|' /etc/xrdp/startwm.sh
-sed -i 's|^exec /bin/sh /etc/X11/Xsession|# &|' /etc/xrdp/startwm.sh
+sudo sed -i 's|^test -x /etc/X11/Xsession && exec /etc/X11/Xsession|# &|' /etc/xrdp/startwm.sh
+sudo sed -i 's|^exec /bin/sh /etc/X11/Xsession|# &|' /etc/xrdp/startwm.sh
 
 sudo /etc/init.d/xrdp start
