69a4c3698a971ddef3632d75d2f41158730977ca
awkirin <awkirin@gmail.com>
2025-09-02 13:09:40 +0300

    Update config-ufw.sh

--- linux/ubuntu/config-ufw.sh
+++ linux/ubuntu/config-ufw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script should be run via curl:
-#   sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/awkirin/awk-snippets/HEAD/bin/config-ufw.sh)"
+#   sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/awkirin/awk-snippets/main/linux/ubuntu/config-ufw.sh)"
 #
 # repo: https://github.com/awkirin/awk-snippets
 
