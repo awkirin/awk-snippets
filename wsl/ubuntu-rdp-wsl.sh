3d328856640b1cb5d45b1d848e5f37f8f6f96938
awkirin <awkirin@gmail.com>
2025-10-09 15:00:54 +0300

    Create ubuntu-rdp-wsl.sh

---
+++ wsl/ubuntu-rdp-wsl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# This script should be run via curl:
+#   sudo apt-get -y update && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/awkirin/awk-scripts/main/install-wsl-xrdp-xfce4.sh)"
+#
+# repo: https://github.com/awkirin/awk-scripts
+
+set -euo pipefail
+
+
+#sudo apt-mark hold acpid acpi-support
+sudo apt update && sudo apt upgrade -y
+sudo apt -y install ubuntu-desktop xrdp
+
+
+if [[ ! -f "/etc/xrdp/xrdp.ini.bak" ]]; then
+    sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+fi
+
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+
+sudo tee ~/.xsessionrc > /dev/null <<EOF
+export GNOME_SHELL_SESSION_MODE=ubuntu
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME
+export XDG_DATA_DIRS=/usr/share/ubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+export WAYLAND_DISPLAY=
+export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg
+EOF
+
+sudo systemctl restart xrdp
