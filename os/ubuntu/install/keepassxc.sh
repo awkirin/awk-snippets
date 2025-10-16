fd4f7bf2aa680b1703293d127ee03cfeae1e148f
awkirin <awkirin@gmail.com>
2025-10-16 14:14:24 +0300

    update

--- os/ubuntu/install/keepassxc.sh
+++
@@ -1,5 +0,0 @@
-sudo add-apt-repository ppa:phoerious/keepassxc
-
-sudo apt update
-
-sudo apt install keepassxc
--- os/ubuntu/install/ohmyzsh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y install zsh
-
-CHSH='no' RUNZSH='no' sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-chsh -s "$(which zsh)" "$USER"
