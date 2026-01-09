4ddc966ea3ab618a347f8bd818467107a6b52466
awkirin <awkirin@gmail.com>
2026-01-09 18:53:54 +0300

    Update awk-update.sh

--- scripts/awk-update.sh
+++ scripts/awk-update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+#   sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/awkirin/awk-snippets/refs/heads/main/scripts/awk-update.sh)"
+
+
 brew update
 brew upgrade --force-bottle
 brew upgrade --cask
