7f4b5503c3acd2f613339a08b0917db23be69ff3
awkirin <awkirin@gmail.com>
2026-02-23 17:28:44 +0300

    Enable and start apt daily upgrade timers
    Enable and start daily upgrade timers for apt.

--- os/ubuntu/config/config-auto-updates.sh
+++ os/ubuntu/config/config-auto-updates.sh
@@ -1,2 +1,6 @@
 sudo apt install -y unattended-upgrades
-sudo dpkg-reconfigure -plow unattended-upgrades
+
+sudo systemctl enable apt-daily.timer apt-daily-upgrade.timer
+sudo systemctl start apt-daily.timer apt-daily-upgrade.timer
+
+#sudo dpkg-reconfigure -plow unattended-upgrades
