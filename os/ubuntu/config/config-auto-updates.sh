46563c03abac5b44a476d107f0fab226b254a7da
awkirin <awkirin@gmail.com>
2026-02-23 17:55:26 +0300

    Enhance auto-updates configuration script
    Added additional packages for unattended upgrades and configured automatic reboot settings.

--- os/ubuntu/config/config-auto-updates.sh
+++ os/ubuntu/config/config-auto-updates.sh
@@ -1,4 +1,10 @@
-sudo apt install -y unattended-upgrades
+sudo apt install -y unattended-upgrades distro-info-data powermgmt-base
+
+sudo tee /etc/apt/apt.conf.d/1000awkirin-unattended-upgrades > /dev/null <<EOF
+Unattended-Upgrade::Automatic-Reboot "true";
+Unattended-Upgrade::Automatic-Reboot-WithUsers "true";
+Unattended-Upgrade::Automatic-Reboot-Time "03:00";
+EOF
 
 sudo systemctl enable apt-daily.timer apt-daily-upgrade.timer
 sudo systemctl start apt-daily.timer apt-daily-upgrade.timer
