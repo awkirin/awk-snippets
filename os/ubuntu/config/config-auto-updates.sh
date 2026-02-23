8daa7135056199b34f30bd15131e12816968f83f
awkirin <awkirin@gmail.com>
2026-02-23 17:57:03 +0300

    Configure automatic updates for Ubuntu

--- os/ubuntu/config/config-auto-updates.sh
+++ os/ubuntu/config/config-auto-updates.sh
@@ -1,6 +1,8 @@
 sudo apt install -y unattended-upgrades distro-info-data powermgmt-base
 
 sudo tee /etc/apt/apt.conf.d/1000awkirin-unattended-upgrades > /dev/null <<EOF
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
 Unattended-Upgrade::Automatic-Reboot "true";
 Unattended-Upgrade::Automatic-Reboot-WithUsers "true";
 Unattended-Upgrade::Automatic-Reboot-Time "03:00";
@@ -8,5 +10,3 @@ EOF
 
 sudo systemctl enable apt-daily.timer apt-daily-upgrade.timer
 sudo systemctl start apt-daily.timer apt-daily-upgrade.timer
-
-#sudo dpkg-reconfigure -plow unattended-upgrades
