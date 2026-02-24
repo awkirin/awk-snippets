07f075d02957852605e1ac62288a4c8aa76335a8
awkirin <awkirin@gmail.com>
2026-02-24 14:25:50 +0300

    Create swap.sh

---
+++ os/ubuntu/config/swap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+
+# резервируем место
+#sudo fallocate -l 1G /swapfile
+#sudo chmod 600 /swapfile
+
+# Форматируем и включаем
+#sudo mkswap /swapfile
+#sudo swapon /swapfile
+
+#swapon --show
+
