0d807d08248edc41602c9dba307d3cb59079d15d
awkirin <awkirin@gmail.com>
2026-03-07 21:47:52 +0300

    update

--- ips.sh
+++ ips.sh
@@ -6,19 +6,25 @@ www.google.com
 play.google.com
 ogs.google.com
 notebooklm.google.com
-
 notebooklm.google
-
 gstatic.com
 www.gstatic.com
 ssl.gstatic.com
 fonts.gstatic.com
-
 googleusercontent.com
 lh3.googleusercontent.com
+nffaoalbilbmmfgbnbgppjihopabppdk
+region1.analytics.google.com
 )
 
+output_file="domains.csv"
+> "$output_file"
+
 for d in "${domains[@]}"; do
-#    echo "$d:"
-    dig +short "$d"
+    # Берём только IPv4
+    for ip in $(dig +short "$d" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'); do
+        echo "$ip,EUW,NL,NL" >> "$output_file"
+    done
 done
+
+echo "Saved CSV to $output_file"
