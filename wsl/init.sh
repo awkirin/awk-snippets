63295aa9b9fdf1c4af8b94d0484b3bdf0c8b510d
awkirin <awkirin@gmail.com>
2026-03-08 04:05:38 +0300

    update

--- wsl/init.sh
+++ wsl/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -uEo pipefail
 
 TZ=${TZ:-"Europe/Moscow"}
 USER_NAME=${USER_NAME:-"awkirin"}
@@ -7,16 +7,16 @@ USER_EMAIL=${USER_EMAIL:-"awkirin@gmail.com"}
 USER_HOME=$(eval echo "~${USER_NAME}")
 IS_WSL=$(grep -qiE "(Microsoft|WSL)" /proc/version && echo 1 || echo 0)
 
-# Чтобы исключить зависание.
-export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive # Чтобы исключить зависание.
 
 function log_info() {
     local msg="$1"
     local timestamp
     timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-    # Зеленый цвет для информации
     echo -e "\033[1;32m[INFO] [$timestamp]\033[0m $msg ================================================================="
 }
+
+sudo timedatectl set-timezone "${TZ}"
 function add_apt_yandex_mirrors() {
   log_info "add apt yandex mirrors"
   sudo tee /etc/apt/sources.list.d/1000-yandex.list > /dev/null <<EOF
@@ -25,7 +25,15 @@ deb https://mirror.yandex.ru/ubuntu/ $(lsb_release -cs)-updates main restricted 
 deb https://mirror.yandex.ru/ubuntu/ $(lsb_release -cs)-backports main restricted universe multiverse
 deb https://mirror.yandex.ru/ubuntu/ $(lsb_release -cs)-security main restricted universe multiverse
 EOF
-}
+}; #add_apt_yandex_mirrors
+sudo tee /etc/apt/apt.conf.d/80-retries > /dev/null <<EOF
+  Acquire::Retries "5";
+EOF
+sudo apt update -y && sudo apt upgrade -y #&& sudo apt full-upgrade -y
+sudo apt install -y fzf coreutils dnsutils curl wget zsh age zip unzip gh htop jq build-essential ca-certificates net-tools
+
+sudo apt install -y ansible nautilus
+
 function config_wsl() {
   if [[ "$IS_WSL" -eq 1 ]]; then
     log_info "wsl config"
@@ -46,90 +54,27 @@ options = "metadata,umask=22,fmask=11"
 #mountFsTab = true
 EOF
   fi
-}
-function install_brew() {
-  if command -v brew >/dev/null 2>&1; then
-    return
-  fi
-  NONINTERACTIVE=1 sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  sudo apt install build-essential
-  brew install gcc
-}
-function install_sops(){
-  log_info "sops"
-  SOPS_LATEST_VERSION=$(curl -s https://api.github.com/repos/getsops/sops/releases/latest | jq -r ".tag_name")
-  sudo curl -L -o /usr/local/bin/sops "https://github.com/getsops/sops/releases/download/$SOPS_LATEST_VERSION/sops-$SOPS_LATEST_VERSION.linux.amd64"
-  sudo chmod +x /usr/local/bin/sops
+}; config_wsl
+
+function install_openssh_client() {
+  echo "openssh-client"
+  sudo apt install -y openssh-client
 
   if [[ "$IS_WSL" -eq 1 ]]; then
-    if [[ -f "/mnt/c/Users/$USER_NAME/.config/sops/age/keys.txt" ]]; then
-      mkdir -p "$USER_HOME/.config/sops/age"
-      cp "/mnt/c/Users/$USER_NAME/.config/sops/age/keys.txt" "$USER_HOME/.config/sops/age/keys.txt" 2>/dev/null || true
-      chmod 700 "$USER_HOME/.config/sops"
-      chmod 600 "$USER_HOME/.config/sops/age/keys.txt" 2>/dev/null || true
+    log_info "wsl ssh-agent"
+    WSL2_SSH_AGENT_PATH="/usr/local/bin/wsl2-ssh-agent"
+    sudo curl -L -o "$WSL2_SSH_AGENT_PATH" https://github.com/mame/wsl2-ssh-agent/releases/latest/download/wsl2-ssh-agent
+    sudo chmod 755 "$WSL2_SSH_AGENT_PATH"
+
+    if [[ -f "/mnt/c/Users/$USER_NAME/.ssh/config" ]]; then
+      log_info "wsl ssh config"
+      mkdir -p "$USER_HOME/.ssh"
+      cp "/mnt/c/Users/$USER_NAME/.ssh/config" "$USER_HOME/.ssh/config" 2>/dev/null || true
+      chmod 700 "$USER_HOME/.ssh"
+      chmod 600 "$USER_HOME/.ssh/config" 2>/dev/null || true
     fi
   fi
-}
-function install_ohmyzsh() {
-  log_info "install oh-my-zsh"
-  sudo apt install zsh -y
-  sudo -u "${USER_NAME}" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  sudo chsh -s "$(which zsh)" "${USER_NAME}"
-
-  log_info "custom config oh-my-zsh"
-  sudo -u "${USER_NAME}" tee "$USER_HOME/.zshrc-custom" > /dev/null <<EOF
-plugins=(git brew ssh-agent)
-EOF
-  sudo -u "${USER_NAME}" sed -i '/^plugins=/c\source $HOME/.zshrc-custom' ~/.zshrc
-
-  if [[ "$IS_WSL" -eq 1 ]]; then
-    log_info "wsl2-ssh-agent"
-    sudo -u "${USER_NAME}" tee "$USER_HOME/.oh-my-zsh/custom/wsl2-ssh-agent.zsh" > /dev/null <<EOF
-eval $(wsl2-ssh-agent)
-EOF
-  fi
-}
-function install_docker() {
-  if [[ "$IS_WSL" -eq 0 ]]; then
-    log_info "docker"
-    sudo sh -c "$(curl -fsSL get.docker.com)"
-    sudo usermod -aG docker "${USER_NAME}"
-    systemctl enable docker
-    systemctl start docker
-  fi
-}
-function install_lando() {
-  if [[ "$IS_WSL" -eq 0 ]]; then
-    log_info "lando"
-    sudo -u "${USER_NAME}" /bin/bash -c "$(curl -fsSL get.lando.dev/setup-lando.sh) --yes"
-  fi
-}
-function install_google_chrome() {
-  if [[ "$IS_WSL" -eq 0 ]]; then
-    log_info "google chrome"
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-    sudo apt update -y
-    sudo apt install -y google-chrome-stable
-  fi
-}
-function install_keepassxc() {
-  if [[ "$IS_WSL" -eq 0 ]]; then
-    log_info "keepass-xc"
-    sudo add-apt-repository -y ppa:phoerious/keepassxc
-    sudo apt update -y
-    sudo apt install -y keepassxc
-  fi
-}
-function install_vagrant() {
-  if [[ "$IS_WSL" -eq 0 ]]; then
-    log_info "vagrant"
-    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-    sudo apt update
-    sudo apt install vagrant
-  fi
-}
+}; install_openssh_client
 function install_git() {
   log_info "git"
   sudo apt install git -y
@@ -150,26 +95,86 @@ function install_git() {
 [color]
   ui = auto
 EOF
-}
-function install_openssh_client() {
-  sudo apt install -y openssh-client
+}; install_git
+function install_docker() {
+  if [[ "$IS_WSL" -eq 0 ]]; then
+    log_info "docker"
+    sudo sh -c "$(curl -fsSL get.docker.com)"
+    sudo usermod -aG docker "${USER_NAME}"
+    systemctl enable docker
+    systemctl start docker
+  fi
+}; install_docker
+function install_lando() {
+  if [[ "$IS_WSL" -eq 0 ]]; then
+    log_info "lando"
+    sudo -u "${USER_NAME}" /bin/bash -c "$(curl -fsSL get.lando.dev/setup-lando.sh) --yes"
+  fi
+}; install_lando
+function install_google_chrome() {
+  if [[ "$IS_WSL" -eq 0 ]]; then
+    log_info "google chrome"
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt update -y
+    sudo apt install -y google-chrome-stable
+  fi
+}; install_google_chrome
+function install_keepassxc() {
+  if [[ "$IS_WSL" -eq 0 ]]; then
+    log_info "keepass-xc"
+    sudo add-apt-repository -y ppa:phoerious/keepassxc
+    sudo apt update -y
+    sudo apt install -y keepassxc
+  fi
+}; install_keepassxc
+function install_vagrant() {
+  if [[ "$IS_WSL" -eq 0 ]]; then
+    log_info "vagrant"
+    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update
+    sudo apt install vagrant
+  fi
+}; install_vagrant
+function install_ohmyzsh() {
+  log_info "install oh-my-zsh"
+  sudo apt install zsh -y
+  sudo -u "${USER_NAME}" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sudo chsh -s "$(which zsh)" "${USER_NAME}"
+
+  log_info "install plugins"
+  sudo -u "${USER_NAME}" git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  sudo -u "${USER_NAME}" git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+  log_info "custom config oh-my-zsh"
+  sudo -u "${USER_NAME}" tee "$USER_HOME/.zshrc-custom" > /dev/null <<EOF
+plugins=(
+z
+sudo
+dotenv
+git-auto-fetch
+brew
+fzf
+command-not-found
+colored-man-pages
+ssh-agent
+history
+history-substring-search
+common-aliases
+zsh-autosuggestions
+zsh-syntax-highlighting
+)
+EOF
+  sudo -u "${USER_NAME}" sed -i '/^plugins=/c\source $HOME/.zshrc-custom' ~/.zshrc
 
   if [[ "$IS_WSL" -eq 1 ]]; then
-    log_info "wsl"
-    log_info "ssh-agent"
-    WSL2_SSH_AGENT_PATH="/usr/local/bin/wsl2-ssh-agent"
-    sudo curl -L -o "$WSL2_SSH_AGENT_PATH" https://github.com/mame/wsl2-ssh-agent/releases/latest/download/wsl2-ssh-agent
-    sudo chmod 755 "$WSL2_SSH_AGENT_PATH"
-
-    if [[ -f "/mnt/c/Users/$USER_NAME/.ssh/config" ]]; then
-      log_info "ssh config"
-      mkdir -p "$USER_HOME/.ssh"
-      cp "/mnt/c/Users/$USER_NAME/.ssh/config" "$USER_HOME/.ssh/config" 2>/dev/null || true
-      chmod 700 "$USER_HOME/.ssh"
-      chmod 600 "$USER_HOME/.ssh/config" 2>/dev/null || true
-    fi
+    log_info "wsl2-ssh-agent"
+    sudo -u "${USER_NAME}" tee "$USER_HOME/.oh-my-zsh/custom/wsl2-ssh-agent.zsh" > /dev/null <<EOF
+eval $(wsl2-ssh-agent)
+EOF
   fi
-}
+}; install_ohmyzsh
 function install_xrdp() {
   sudo apt install xrdp -y
 
@@ -192,29 +197,50 @@ EOF
 
     sudo systemctl restart xrdp
   fi
-}
+}; #install_xrdp
 
-sudo timedatectl set-timezone "${TZ}"
-#add_apt_yandex_mirrors
-#sudo tee /etc/apt/apt.conf.d/80-retries > /dev/null <<EOF
-#Acquire::Retries "5";
-#EOF
-sudo apt update -y && sudo apt upgrade -y #&& sudo apt full-upgrade -y
-sudo apt install -y coreutils dnsutils curl wget zsh age zip unzip gh htop jq build-essential ca-certificates net-tools
+sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean
 
-config_wsl
-install_brew
-install_openssh_client
-install_git
-install_sops
-install_docker
-install_lando
-install_google_chrome
-install_keepassxc
-install_vagrant
-install_ohmyzsh
-sudo apt install -y ansible nautilus
-#install_xrdp
+function install_brew() {
+  echo "build-essential"
+  sudo apt install -y build-essential
+
+  echo "brew"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+
+  echo "install gcc"
+  brew install gcc
+}; install_brew
+function install_sops() {
+  log_info "sops"
+  brew install sops
+#  SOPS_LATEST_VERSION=$(curl -s https://api.github.com/repos/getsops/sops/releases/latest | jq -r ".tag_name")
+#  sudo curl -L -o /usr/local/bin/sops "https://github.com/getsops/sops/releases/download/$SOPS_LATEST_VERSION/sops-$SOPS_LATEST_VERSION.linux.amd64"
+#  sudo chmod +x /usr/local/bin/sops
+
+  if [[ "$IS_WSL" -eq 1 ]]; then
+    if [[ -f "/mnt/c/Users/$USER_NAME/.config/sops/age/keys.txt" ]]; then
+      mkdir -p "$USER_HOME/.config/sops/age"
+      cp "/mnt/c/Users/$USER_NAME/.config/sops/age/keys.txt" "$USER_HOME/.config/sops/age/keys.txt" 2>/dev/null || true
+      chmod 700 "$USER_HOME/.config/sops"
+      chmod 600 "$USER_HOME/.config/sops/age/keys.txt" 2>/dev/null || true
+    fi
+  fi
+}; install_sops
+function install_nvm() {
+    brew install nvm
+    sudo -u "${USER_NAME}" tee "$USER_HOME/.oh-my-zsh/custom/nvm.zsh" > /dev/null <<EOF
+export NVM_DIR="$HOME/.nvm"
+    [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+    [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
+EOF
+  source "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+}; install_nvm
+function install_node() {
+    nvm install --lts
+    corepack enable yarn
+    corepack enable pnpm
+}; install_node
 
 log_info "end"
-sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean && exit
