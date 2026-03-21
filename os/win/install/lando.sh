54dc5041989fd5afdd49c343a8d423ea9a11c2f4
awkirin <awkirin@gmail.com>
2026-03-22 02:25:30 +0300

    update

--- os/win/install/lando.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -fsSL https://get.lando.dev/setup-lando.sh -o tmp-setup-lando.sh
-chmod +x ./tmp-setup-lando.sh
-bash ./tmp-setup-lando.sh -y
-rm ./tmp-setup-lando.sh
-
-# OSX
-# security add-trusted-cert -r trustRoot -k ~/Library/Keychains/login.keychain-db ~/.lando/certs/LandoCA.crt
