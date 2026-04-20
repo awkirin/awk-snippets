bd329d4a0e06a99163a0f4bca5a0babdd4fbf92a
awkirin <awkirin@gmail.com>
2026-04-20 14:39:16 +0300

    Update firefox.sh

--- os/ubuntu/install/firefox.sh
+++ os/ubuntu/install/firefox.sh
@@ -1,10 +1,13 @@
 # удалить snap
-#sudo apt autoremove --purge snapd
-#sudo apt purge snapd
-#sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd
+sudo apt autoremove --purge snapd -y
+sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd
+sudo apt-mark hold snapd
+
 
 sudo apt install extrepo -y
-sudo extrepo enable mozilla -y
+
+
+sudo extrepo enable mozilla
 sudo apt update -y
 
 sudo tee /etc/apt/preferences.d/firefox <<EOF
@@ -13,5 +16,4 @@ Pin: origin packages.mozilla.org
 Pin-Priority: 1001
 EOF
 
-sudo snap remove firefox -y
 sudo apt install firefox -y
